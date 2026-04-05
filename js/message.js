3c048040b7ecd1df6f9a509a608b5570510b40cd
vabhi2024 <vabhi2024@gmail.com>
2026-04-05 13:29:05 +0530

    Add files via upload

---
+++ js/message.js
@@ -0,0 +1,33 @@
+document.getElementById("jobForm").addEventListener("submit", async function (e) {
+    e.preventDefault(); 
+
+    const formData = new FormData(this);
+
+    // ⭐ Access Key added hidden in JS
+    formData.append("access_key", "fdff9149-19fc-4bf3-8a40-f83b405de0d0");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+        document.getElementById("popup").style.display = "flex";
+        this.reset();
+    } else {
+        alert("Error: " + data.message);
+    }
+});
+
+
+// ---------------- POPUP CLOSE ----------------
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+window.onclick = function(e){
+    const popup = document.getElementById("popup");
+    if (e.target === popup) popup.style.display = "none";
+}
